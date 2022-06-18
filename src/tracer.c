e7b3fed92aae31e98a6af178afefb946e7e47f60
Alexios Zavras <github@zvr.gr>
2022-06-18 12:48:44 +0200

    Added formatted source files
    Signed-off-by: Alexios Zavras <github@zvr.gr>

--- src/tracer.c
+++ src/tracer.c
@@ -1,3 +1,4 @@
+
 /*
 	Copyright (C) 2022 Valasiadis Fotios
 	SPDX-License-Identifier: LGPL-2.1-or-later
@@ -22,9 +23,9 @@
 void
 tracee_main(char **argv)
 {
-	ptrace(PTRACE_TRACEME, NULL, NULL, NULL);
-	execvp(*argv, argv);
-	FATAL_ERROR; // exec shouldn't return.
+    ptrace(PTRACE_TRACEME, NULL, NULL, NULL);
+    execvp(*argv, argv);
+    FATAL_ERROR;		       // exec shouldn't return.
 }
 
 /*
@@ -33,10 +34,12 @@ tracee_main(char **argv)
 int
 has_end_of_str(const char *buffer, size_t size)
 {
-    for(size_t i = 0; i < size; ++i) {
-        if(buffer[i] == '\0') {
-            return 1;
-        }
+    for (size_t i = 0; i < size; ++i)
+    {
+	if (buffer[i] == '\0')
+	{
+	    return 1;
+	}
     }
 
     return 0;
@@ -51,92 +54,114 @@ const char *
 read_str_from_process(char *addr, pid_t pid)
 {
     static long buffer[MAXPATHLEN];
-    static const char *cbuffer = (char *)buffer; // For readability
+    static const char *cbuffer = (char *) buffer;	// For readability
     size_t size = 0;
-    
-    do {
-        buffer[size] = ptrace(PTRACE_PEEKDATA, pid, addr + size * sizeof(long), NULL);
-    } while(!has_end_of_str(cbuffer + size, sizeof(long)) && ++size != MAXPATHLEN);
 
-    if(size == MAXPATHLEN && cbuffer[size * sizeof(long) - 1] != '\0') {
-        fprintf(stderr, "maximum file path size of %ld exceeded", MAXPATHLEN);
-        _exit(1);
+    do
+    {
+	buffer[size] =
+		ptrace(PTRACE_PEEKDATA, pid, addr + size * sizeof (long), NULL);
+    } while (!has_end_of_str(cbuffer + size, sizeof (long)) &&
+	     ++size != MAXPATHLEN);
+
+    if (size == MAXPATHLEN && cbuffer[size * sizeof (long) - 1] != '\0')
+    {
+	fprintf(stderr, "maximum file path size of %ld exceeded", MAXPATHLEN);
+	_exit(1);
     }
 
     return cbuffer;
 }
 
 void
-handle_syscall(pid_t pid, const struct ptrace_syscall_info *entry, const struct ptrace_syscall_info *exit, files *buffer)
+handle_syscall(pid_t pid, const struct ptrace_syscall_info *entry,
+	       const struct ptrace_syscall_info *exit, files * buffer)
 {
-    if(exit->exit.rval < 0) {
-        return; // return on syscall failure
+    if (exit->exit.rval < 0)
+    {
+	return;			       // return on syscall failure
     }
 
     int syscall = entry->entry.nr;
 
-    if(syscall == SYS_open || syscall == SYS_creat) {
-        puts(read_str_from_process((char *)entry->entry.args[0], pid));
-    } else if(syscall == SYS_openat) {
-        puts(read_str_from_process((char *)entry->entry.args[1], pid));
+    if (syscall == SYS_open || syscall == SYS_creat)
+    {
+	puts(read_str_from_process((char *) entry->entry.args[0], pid));
+    } else if (syscall == SYS_openat)
+    {
+	puts(read_str_from_process((char *) entry->entry.args[1], pid));
     }
 }
 
 int
-tracer_main(pid_t pid, files *buffer)
+tracer_main(pid_t pid, files * buffer)
 {
-	waitpid(pid, NULL, 0);
-	ptrace(PTRACE_SETOPTIONS, pid, NULL, PTRACE_O_EXITKILL | PTRACE_O_TRACESYSGOOD);
+    waitpid(pid, NULL, 0);
+    ptrace(PTRACE_SETOPTIONS, pid, NULL,
+	   PTRACE_O_EXITKILL | PTRACE_O_TRACESYSGOOD);
 
-	struct ptrace_syscall_info entry;
+    struct ptrace_syscall_info entry;
     struct ptrace_syscall_info exit;
-	while(1) {
-		if(ptrace(PTRACE_SYSCALL, pid, NULL, NULL) < 0) {
-			FATAL_ERROR;
-		}
-		if(waitpid(pid, NULL, 0) < 0) {
-			FATAL_ERROR;
-		}
 
-		if(ptrace(PTRACE_GET_SYSCALL_INFO, pid, (void *)sizeof(entry), &entry) == -1) {
-			FATAL_ERROR;
-		}
-
-		if(ptrace(PTRACE_SYSCALL, pid, NULL, NULL) < 0) {
-			FATAL_ERROR;
-		}
-		if(waitpid(pid, NULL, 0) < 0) {
-			FATAL_ERROR;
-		}
-
-		if(ptrace(PTRACE_GET_SYSCALL_INFO, pid, (void *)sizeof(exit), &exit) == -1) {
-			if(errno == ESRCH) {
-				return exit.exit.rval;
-			}
-			FATAL_ERROR;
-		}
-
-        handle_syscall(pid, &entry, &exit, buffer);
+    while (1)
+    {
+	if (ptrace(PTRACE_SYSCALL, pid, NULL, NULL) < 0)
+	{
+	    FATAL_ERROR;
 	}
+	if (waitpid(pid, NULL, 0) < 0)
+	{
+	    FATAL_ERROR;
+	}
+
+	if (ptrace
+	    (PTRACE_GET_SYSCALL_INFO, pid, (void *) sizeof (entry),
+	     &entry) == -1)
+	{
+	    FATAL_ERROR;
+	}
+
+	if (ptrace(PTRACE_SYSCALL, pid, NULL, NULL) < 0)
+	{
+	    FATAL_ERROR;
+	}
+	if (waitpid(pid, NULL, 0) < 0)
+	{
+	    FATAL_ERROR;
+	}
+
+	if (ptrace(PTRACE_GET_SYSCALL_INFO, pid, (void *) sizeof (exit), &exit)
+	    == -1)
+	{
+	    if (errno == ESRCH)
+	    {
+		return exit.exit.rval;
+	    }
+	    FATAL_ERROR;
+	}
+
+	handle_syscall(pid, &entry, &exit, buffer);
+    }
 }
 
 int
-get_files_used(char **argv, files *buffer)
+get_files_used(char **argv, files * buffer)
 {
-	pid_t pid;
+    pid_t pid;
 
-	switch(pid = fork()) {
-		case -1:
-			FATAL_ERROR;
-		case 0:
-			tracee_main(argv + 1);
-	}
-	
-	return tracer_main(pid, buffer);
+    switch (pid = fork())
+    {
+	case -1:
+	    FATAL_ERROR;
+	case 0:
+	    tracee_main(argv + 1);
+    }
+
+    return tracer_main(pid, buffer);
 }
 
 void
-free_files(files *buffer)
+free_files(files * buffer)
 {
-    //TODO
+    // TODO
 }
