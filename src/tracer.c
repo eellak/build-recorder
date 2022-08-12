0773ba3e6300df8cc966b79c855563cd4398b83d
Valasiadis Fotios <fvalasiad@csd.auth.gr>
2022-08-12 10:02:58 +0300

    now records process exit

--- src/tracer.c
+++ src/tracer.c
@@ -297,6 +297,7 @@ tracer_main(pid_t pid)
 	} else if (WIFEXITED(status))  // child process exited
 	{
 	    --running;
+	    record_process_end(pid);
 	} else {
 	    error(EXIT_FAILURE, errno, "expected stop or tracee death\n");
 	}
