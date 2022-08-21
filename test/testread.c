a9776a0bee416de9164cbb3b9e320990b375b655
Alexios Zavras <github@zvr.gr>
2022-08-21 14:49:19 +0200

    Add sample test runs
    Signed-off-by: Alexios Zavras <github@zvr.gr>

--- test/testread.c
+++ test/testread.c
@@ -20,11 +20,11 @@
 void
 read_file(char *fpath)
 {
-    int             fd = open(fpath, O_RDONLY);
+    int fd = open(fpath, O_RDONLY);
 
     (void) printf("[%jd] opened(%s)= %d\n", (intmax_t) getpid(), fpath, fd);
 
-    unsigned int    secs = random() % MAX_TIME_FILE_OPEN;
+    unsigned int secs = random() % MAX_TIME_FILE_OPEN;
 
     sleep(secs);
 
@@ -34,13 +34,13 @@ read_file(char *fpath)
     return;
 }
 
-char           *dirpaths[] =
+char *dirpaths[] =
 	{ ".", "/usr/local/include", "/usr/include", "/usr/include/sys", NULL };
 
 void
 setup_dirpaths(void)
 {
-    char           *cpp_flags = getenv("CPPFLAGS");
+    char *cpp_flags = getenv("CPPFLAGS");
 
     // TODO: set up the global variable according to environent
     return;
@@ -52,7 +52,7 @@ open_incl_file(char *fn)
     if (*fn == '/') {		       // absolute path
 	read_file(fn);
     } else {
-	char            buf[PATH_MAX];
+	char buf[PATH_MAX];
 
 	for (char **d = dirpaths; *d; d++) {
 	    sprintf(buf, "%s/%s", *d, fn);
